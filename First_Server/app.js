0cf26fad0bd46a0eb9dc428ee27c0f7e431edc17
Mullefyren <57482909+Mullefyren@users.noreply.github.com>
2021-02-12 13:06:23 +0100

    API færdig

--- First_Server/app.js
+++ First_Server/app.js
@@ -5,12 +5,38 @@ const app = express();
 // your are allowed to hardcode data the id
 
 app.get("/", (req, res) => {
-  res.send({ message: "First call to /" });
+  res.send({
+    greeting:
+      "Hi! Welcome to my API of students. You can got to /classmates to see a list of students, and you can also go to /students/(id of a student) to see a specific student.",
+  });
 });
 
-app.get("/anotherpath", (req, res) => {
-  const id = 1;
-  res.send({ data: "Some other things" });
+const students = [
+  {
+    id: 1,
+    name: "William",
+  },
+  {
+    id: 2,
+    name: "Christopher",
+  },
+  {
+    id: 3,
+    name: "Jakob",
+  },
+  {
+    id: 4,
+    name: "Cecilie",
+  },
+];
+
+app.get("/classmates", (req, res) => {
+  res.send(students);
 });
 
-app.listen(8080); // conventionel http developmentg port
+app.get("/classmates/:id", (req, res) => {
+  let studList = students.filter((stud) => stud.id == req.params.id);
+  res.send(studList);
+});
+
+app.listen(8080); // conventionel http development port
