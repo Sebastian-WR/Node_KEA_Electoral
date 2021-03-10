9407e37ed15685c7841b65e43ba5aed0a84c1d8b
Mullefyren <57482909+Mullefyren@users.noreply.github.com>
2021-03-10 16:33:07 +0100

    Added mandatory folder with express and index html

---
+++ Mandatory_Assignment/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+let path = __dirname + '/public/';
+const PORT = 8080;
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path + 'index.html');
+});
+
+app.get("/:id", (req, res) => {
+    res.send();
+});
+
+app.post("/", (req, res) => {
+    res.send();
+});
+
+app.patch("/", (req, res) => {
+    res.send();
+});
+
+app.delete("/", (req, res) => {
+    res.send();
+});
+
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("This server is running on port: " + PORT);
+    }
+});
