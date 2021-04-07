51c5a33c0c117ead39ea6491c1ebaff922720bb7
Mullefyren <57482909+Mullefyren@users.noreply.github.com>
2021-04-07 16:36:21 +0200

    Google serve with fetch created and added to world wide web express folder

--- 6._Express_World_Wide_Web/app.js
+++ 6._Express_World_Wide_Web/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080; // lavet om så den tager provess environment port eller 8080 afhænglkig af hvordan men starter den, med npm run start-dev eller nodemon.cmd app.js
+app.use(express.static('public'));
+const fetch = require("node-fetch");
+const fs = require('fs');
 
 app.get("/", (req, res) => {
     //__dirname exists in Node.js
     // res.sendFile is a method in Express
 
     // todo serve the welcome page on root
-    res.sendFile(__dirname + "/public/welcome.html");
+    res.sendFile(__dirname + "/public/welcome/welcome.html");
 });
 
 app.get("/pizza", (req, res) => {
@@ -24,6 +27,32 @@ app.get("/pub",(req, res) => {
     } else {
         res.send({ doorman: "Sorry you don't have money for whiskey."})
     } 
+});
+
+app.get("/catfacts", (req, res) => {
+    res.sendFile(__dirname + "/public/catfacts/catfacts.html")
+});
+
+// this fetches the html and sends it right back to the document/window
+app.get("/proxy1", (req, res) => {
+    fetch('https://www.google.dk/').then(function (res) {
+    return res.text();
+    }).then(function (html) {
+    res.send(html);
+    });
+});
+
+// creates a file and writes the html from google that gets fetched to the file. and serves the html file. it doesnt work if the file is not created at first though because of the asynchronism. 
+app.get("/proxy", (req, res) => {
+    (async function () {
+        const response = await fetch("https://www.google.dk/");
+        const result = await response.text();
+        fs.writeFile('google.html', result, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          });
+    })()
+    res.sendFile(__dirname + "/google.html")
 });
 
  // serve more html pages and make new route for it :D 
